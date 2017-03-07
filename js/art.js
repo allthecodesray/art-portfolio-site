ed34bc09558ae9ed147b92a29d9ffab31dae0540
Fabien <Fabien@IBM805-R90HLVHM.mul.ie.ibm.com>
2017-03-07 18:35:49 +0000

    adding a js file

---
+++ js/art.js
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+
+var make_button_active = function()
+{
+  //Get item siblings
+  var siblings =($(this).siblings());
+
+  //Remove active class on all buttons
+  siblings.each(function (index)
+    {
+      $(this).removeClass('active');
+    }
+  )
+
+
+  //Add the clicked button class
+  $(this).addClass('active');
+}
+
+//Attach events to menu
+$(document).ready(
+  function()
+  {
+    $(".menu li").click(make_button_active);
+  }  
+)
+
+</script>
